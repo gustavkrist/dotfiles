afca13320ba22d5defc86a869f0e2a6e5bac8483
Gustav Kristensen <gustavkrist@gmail.com>
2022-05-17 20:53:28 +0200

    add vscode space, move to next space script

--- .config/yabai/scripts/window-focus-on-destroy.sh
+++ .config/yabai/scripts/window-focus-on-destroy.sh
@@ -1,6 +1,6 @@
 isFocused=$(yabai -m query --windows --window | jq -re ".id")
 echo $isFocused
-if [[ -z "$isFocused" ]]; then # -z >> true if it's null
-    $(yabai -m window --focus $(yabai -m query --windows | jq -re ".[] | select((.visible == 1) and .focused != 1).id" | head -n 1))
+if [[ -z "$isFocused" ]]; then
+    $(yabai -m window --focus $(yabai -m query --windows | jq -re '.[] | select((."is-visible" == true) and ."has-focus" == false).id' | head -n 1))
     echo hi
 fi
