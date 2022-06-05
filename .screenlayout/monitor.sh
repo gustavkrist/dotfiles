38369608a8d625e0b566205d307b9297e97f319c
Gustav Kristensen <gustavkrist@gmail.com>
2022-06-05 12:03:13 +0200

    add screen layout

---
+++ .screenlayout/monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --mode 2560x1440 --rate 144 --pos 3440x0 --rotate left --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --rate 144 --pos 0x560 --rotate normal --output DP-3 --off
