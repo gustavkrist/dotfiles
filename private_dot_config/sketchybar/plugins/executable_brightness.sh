36f848fef6ff52aa56b8eebae8bddab01bac8093
Gustav Kristensen <gustavkrist@gmail.com>
2024-10-17 22:19:43 +0200

    sketchybar: completely new config

--- private_dot_config/sketchybar/plugins/executable_brightness.sh
+++
@@ -1,2 +0,0 @@
-LABEL=$(/usr/local/bin/brightness -l | awk 'NR==2 {printf("%02.0f", $4*100)}')%
-sketchybar -m --set $NAME label=$LABEL
--- private_dot_config/sketchybar/plugins/executable_clock.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sketchybar -m --set $NAME label="$(date '+%Y-%m-%d-W%W-0%u %H:%M:%S%z')"
-# sketchybar -m --set $NAME label="$(date '+%d/%m %H:%M')"
