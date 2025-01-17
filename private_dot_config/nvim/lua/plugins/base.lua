8c3f4b43b1eb538c1015b401d9e7e628d76f97a6
Gustav Kristensen <gustavkrist@gmail.com>
2025-01-17 12:48:33 +0100

    disable autopairs in snacks picker input

--- private_dot_config/nvim/lua/plugins/base.lua
+++ private_dot_config/nvim/lua/plugins/base.lua
@@ -258,7 +258,7 @@ return {
         javascript = { "string", "template_string" },
         java = false,
       },
-      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
       fast_wrap = {
         map = "<M-e>",
         chars = { "{", "[", "(", '"', "'" },
