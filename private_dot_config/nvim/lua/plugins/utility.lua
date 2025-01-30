88e62b82f283b09b5214094bf3b0fc7970a8dfb0
Gustav Kristensen <gustavkrist@gmail.com>
2025-01-30 18:17:34 +0100

    default wezterm pane direction to right

--- private_dot_config/nvim/lua/plugins/utility.lua
+++ private_dot_config/nvim/lua/plugins/utility.lua
@@ -231,6 +231,7 @@ return {
       vim.g.slime_no_mappings = 1
       vim.g.slime_bracketed_paste = 1
       vim.g.slime_target = "wezterm"
+      vim.g.slime_default_config = { pane_direction = "right" }
     end,
   },
 }
