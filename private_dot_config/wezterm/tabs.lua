d2af9aa25a01856319d0d1a3133b9f1724f491de
Gustav Kristensen <gustavkrist@gmail.com>
2025-02-05 16:12:01 +0100

    wezterm: formatting

--- private_dot_config/wezterm/tabs.lua
+++ private_dot_config/wezterm/tabs.lua
@@ -4,11 +4,11 @@ local M = {}
 
 ---@param config Config
 function M.setup(config)
-  config.use_fancy_tab_bar = true
-  config.tab_bar_at_bottom = false
-  config.hide_tab_bar_if_only_one_tab = wezterm.target_triple:find("windows") == nil
-  config.tab_max_width = 32
-  config.unzoom_on_switch_pane = true
+	config.use_fancy_tab_bar = true
+	config.tab_bar_at_bottom = false
+	config.hide_tab_bar_if_only_one_tab = wezterm.target_triple:find("windows") == nil
+	config.tab_max_width = 32
+	config.unzoom_on_switch_pane = true
 end
 
 return M
