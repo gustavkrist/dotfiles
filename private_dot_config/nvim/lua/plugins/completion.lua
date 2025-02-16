efcf3f815acb0ae09dc79658f487bb7eec7b9f90
Gustav Kristensen <gustavkrist@gmail.com>
2025-02-16 23:42:03 +0100

    nvim: blink config update

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -336,20 +336,22 @@ return {
       snippets = {
         preset = "luasnip",
       },
+      cmdline = {
+        sources = function()
+            local type = vim.fn.getcmdtype()
+            -- Search forward and backward
+            if type == "/" or type == "?" then
+              return { "buffer" }
+            end
+            -- Commands
+            if type == ":" then
+              return { "cmdline" }
+            end
+            return {}
+          end,
+      },
       sources = {
         default = { "lsp", "path", "buffer", "snippets", "lazydev", "copilot" },
-        cmdline = function()
-          local type = vim.fn.getcmdtype()
-          -- Search forward and backward
-          if type == "/" or type == "?" then
-            return { "buffer" }
-          end
-          -- Commands
-          if type == ":" then
-            return { "cmdline" }
-          end
-          return {}
-        end,
         providers = {
           lsp = {
             name = "[LSP]",
