2477dd7069b6e85aa7f358942523a3a79f73acf6
Gustav Kristensen <gustavkrist@gmail.com>
2026-01-30 22:48:13 +0100

    nvim: ctags-lsp platform agnostic

--- private_dot_config/nvim/after/lsp/ctags_lsp.lua
+++ private_dot_config/nvim/after/lsp/ctags_lsp.lua
@@ -1,5 +1,10 @@
+local cmd
+if require("util.os").is_macos() then
+  cmd = { "ctags-lsp", "--ctags-bin=/opt/homebrew/bin/ctags", "--languages=coq" } else
+  cmd = { "ctags-lsp", "--languages=coq" }
+end
 vim.lsp.config("ctags_lsp", {
-  cmd = { "ctags-lsp", "--ctags-bin=/opt/homebrew/bin/ctags", "--languages=coq" },
+  cmd = cmd,
   filetypes = { "coq" },
   root_markers = { ".git" },
 })
